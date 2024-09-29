084bc85c206f6132ad339cc74540de8949f03533
alvine <alvinecom2018@gmail.com>
2024-09-29 09:52:57 +0700

    setup env new host

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     HOST: process.env.DB_HOST || 'localhost',
     USER: process.env.DB_USER || "root",
-    PASSWORD: process.env.DB_PASSWORD || "",
+    PASSWORD: process.env.DB_PASSWORD || "alvine1234",
     DB: process.env.DB_NAME || "marketplace",
     dialect: "mysql",
     pool: {
