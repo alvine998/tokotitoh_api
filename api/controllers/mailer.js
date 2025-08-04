6d37b760f154befde8210909f68fe098343cf354
alvine <alvinecom2018@gmail.com>
2025-08-04 10:23:28 +0700

    handle error sent header

--- api/controllers/mailer.js
+++ api/controllers/mailer.js
@@ -1,139 +1,151 @@
-const nodemailer = require('nodemailer');
-const db = require('../models');
-const { generateRandomSixDigitNumber } = require('../../utils');
-const users = db.users
-const Op = db.Sequelize.Op
-require('dotenv').config();
+const nodemailer = require("nodemailer");
+const db = require("../models");
+const { generateRandomSixDigitNumber } = require("../../utils");
+const users = db.users;
+const Op = db.Sequelize.Op;
+require("dotenv").config();
 
 exports.sendEmail = async (req, res) => {
-    try {
-        ['from', 'to']?.map(value => {
-            if (!req.body[value]) {
-                return res.status(400).send({
-                    status: "error",
-                    error_message: "Parameter tidak lengkap " + value,
-                    code: 400
-                })
-            }
-        })
-
-        const existUser = await users.findOne({
-            where: {
-                email: { [Op.eq]: req.body.to },
-                role: { [Op.eq]: 'customer' },
-                deleted: { [Op.eq]: 0 }
-            }
-        })
-
-        if (!existUser) {
-            return res.status(400).send({
-                status: "not found",
-                error_message: "Email belum terdaftar",
-                code: 400
-            })
-        }
-        const otp = generateRandomSixDigitNumber()
-        await users.update({
-            reset_otp: otp,
-            reset_status: existUser.reset_status + 1
-        }, {
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: existUser.id }
-            }
-        })
-        const transport = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
-            }
+  try {
+    const requiredFields = ["from", "to"];
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).send({
+          status: "error",
+          error_message: "Parameter tidak lengkap " + field,
+          code: 400,
         });
-        const payload = {
-            ...req.body,
-            subject: "Reset Password",
-            text: `Gunakan Kode OTP ini untuk verifikasi pemulihan password: ${otp}`,
-        };
-        transport.sendMail(payload, function (error, info) {
-            if (error) throw Error(error);
-            console.log('email send successfully');
-            console.log(info);
-        })
-        return res.status(200).send({
-            status: "success",
-            message: "Email Sent",
-            items: existUser,
-            code: 200
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Server mengalami gangguan!", error: error })
-        return
+      }
     }
+
+    const existUser = await users.findOne({
+      where: {
+        email: { [Op.eq]: req.body.to },
+        role: { [Op.eq]: "customer" },
+        deleted: { [Op.eq]: 0 },
+      },
+    });
+
+    if (!existUser) {
+      return res.status(400).send({
+        status: "not found",
+        error_message: "Email belum terdaftar",
+        code: 400,
+      });
+    }
+    const otp = generateRandomSixDigitNumber();
+    await users.update(
+      {
+        reset_otp: otp,
+        reset_status: existUser.reset_status + 1,
+      },
+      {
+        where: {
+          deleted: { [Op.eq]: 0 },
+          id: { [Op.eq]: existUser.id },
+        },
+      }
+    );
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+    const payload = {
+      ...req.body,
+      subject: "Reset Password",
+      text: `Gunakan Kode OTP ini untuk verifikasi pemulihan password: ${otp}`,
+    };
+    transport.sendMail(payload, function (error, info) {
+      if (error) throw Error(error);
+      console.log("email send successfully");
+      console.log(info);
+    });
+    return res.status(200).send({
+      status: "success",
+      message: "Email Sent",
+      items: existUser,
+      code: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Server mengalami gangguan!", error: error });
+    return;
+  }
 };
 
 exports.sendEmailVerificationRegister = async (req, res) => {
-    try {
-        ['from', 'to']?.map(value => {
-            if (!req.body[value]) {
-                return res.status(400).send({
-                    status: "error",
-                    error_message: "Parameter tidak lengkap " + value,
-                    code: 400
-                })
-            }
-        })
-
-        const existUser = await users.findOne({
-            where: {
-                email: { [Op.eq]: req.body.to },
-                role: { [Op.eq]: 'customer' },
-                deleted: { [Op.eq]: 0 }
-            }
-        })
-
-        if (!existUser) {
-            return res.status(400).send({
-                status: "not found",
-                error_message: "Email belum terdaftar",
-                code: 400
-            })
-        }
-        const otp = generateRandomSixDigitNumber()
-        await users.update({
-            email_otp: otp
-        }, {
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: existUser.id }
-            }
-        })
-        const transport = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD
-            }
+  try {
+    const requiredFields = ["from", "to"];
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).send({
+          status: "error",
+          error_message: "Parameter tidak lengkap " + field,
+          code: 400,
         });
-        const payload = {
-            ...req.body,
-            subject: "Register Verification Tokotitoh",
-            text: `Gunakan Kode OTP ini untuk verifikasi pendaftaran di Tokotitoh: ${otp}`,
-        };
-        transport.sendMail(payload, function (error, info) {
-            if (error) throw Error(error);
-            console.log('email send successfully');
-            console.log(info);
-        })
-        return res.status(200).send({
-            status: "success",
-            message: "Email Sent",
-            items: existUser,
-            code: 200
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Server mengalami gangguan!", error: error })
-        return
+      }
     }
+
+    const existUser = await users.findOne({
+      where: {
+        email: { [Op.eq]: req.body.to },
+        role: { [Op.eq]: "customer" },
+        deleted: { [Op.eq]: 0 },
+      },
+    });
+
+    if (!existUser) {
+      return res.status(400).send({
+        status: "not found",
+        error_message: "Email belum terdaftar",
+        code: 400,
+      });
+    }
+    const otp = generateRandomSixDigitNumber();
+    await users.update(
+      {
+        email_otp: otp,
+      },
+      {
+        where: {
+          deleted: { [Op.eq]: 0 },
+          id: { [Op.eq]: existUser.id },
+        },
+      }
+    );
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+      },
+    });
+    const payload = {
+      ...req.body,
+      subject: "Register Verification Tokotitoh",
+      text: `Gunakan Kode OTP ini untuk verifikasi pendaftaran di Tokotitoh: ${otp}`,
+    };
+    transport.sendMail(payload, function (error, info) {
+      if (error) throw Error(error);
+      console.log("email send successfully");
+      console.log(info);
+    });
+    return res.status(200).send({
+      status: "success",
+      message: "Email Sent",
+      items: existUser,
+      code: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Server mengalami gangguan!", error: error });
+    return;
+  }
 };
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,306 +1,340 @@
-
-const db = require('../models')
-const users = db.users
-const Op = db.Sequelize.Op
-const bcrypt = require('bcryptjs')
-require('dotenv').config()
+const db = require("../models");
+const users = db.users;
+const Op = db.Sequelize.Op;
+const bcrypt = require("bcryptjs");
+require("dotenv").config();
 
 // Retrieve and return all notes from the database.
 exports.list = async (req, res) => {
-    try {
-        const size = +req.query.size || 10;
-        const page = +req.query.page || 0;
-        const offset = size * page;
+  try {
+    const size = +req.query.size || 10;
+    const page = +req.query.page || 0;
+    const offset = size * page;
 
-        const result = await users.findAndCountAll({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                partner_code: { [Op.eq]: req.header('x-partner-code') },
-                ...req.query.id && { id: { [Op.eq]: req.query.id } },
-                ...req.query.role && { role: { [Op.in]: req.query.role.split(",") } },
-                ...req.query.isCustomer == '1' && { google_id: { [Op.not]: null } },
-                ...req.query.isCustomer == '0' && { google_id: { [Op.is]: null } },
-                ...req.query.google_id && { google_id: { [Op.eq]: req.query.google_id } },
-                ...req.query.search && {
-                    [Op.or]: [
-                        { name: { [Op.like]: `%${req.query.search}%` } },
-                        { email: { [Op.like]: `%${req.query.search}%` } },
-                        { phone: { [Op.like]: `%${req.query.search}%` } },
-                    ]
-                },
-            },
-            order: [
-                ['created_on', 'DESC'],
-            ],
-            attributes: { exclude: ['deleted', 'password'] },
-            ...req.query.pagination == 'true' && {
-                limit: size,
-                offset: offset
-            }
-        })
-        return res.status(200).send({
-            status: "success",
-            items: result,
-            total_pages: Math.ceil(result.count / size),
-            current_page: page,
-            code: 200
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Server mengalami gangguan!", error: error })
-        return
-    }
+    const result = await users.findAndCountAll({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        partner_code: { [Op.eq]: req.header("x-partner-code") },
+        ...(req.query.id && { id: { [Op.eq]: req.query.id } }),
+        ...(req.query.role && { role: { [Op.in]: req.query.role.split(",") } }),
+        ...(req.query.isCustomer == "1" && { google_id: { [Op.not]: null } }),
+        ...(req.query.isCustomer == "0" && { google_id: { [Op.is]: null } }),
+        ...(req.query.google_id && {
+          google_id: { [Op.eq]: req.query.google_id },
+        }),
+        ...(req.query.search && {
+          [Op.or]: [
+            { name: { [Op.like]: `%${req.query.search}%` } },
+            { email: { [Op.like]: `%${req.query.search}%` } },
+            { phone: { [Op.like]: `%${req.query.search}%` } },
+          ],
+        }),
+      },
+      order: [["created_on", "DESC"]],
+      attributes: { exclude: ["deleted", "password"] },
+      ...(req.query.pagination == "true" && {
+        limit: size,
+        offset: offset,
+      }),
+    });
+    return res.status(200).send({
+      status: "success",
+      items: result,
+      total_pages: Math.ceil(result.count / size),
+      current_page: page,
+      code: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Server mengalami gangguan!", error: error });
+    return;
+  }
 };
 
 exports.create = async (req, res) => {
-    try {
-        ['name', 'password', 'role']?.map(value => {
-            if (!req.body[value]) {
-                return res.status(400).send({
-                    status: "error",
-                    error_message: "Parameter tidak lengkap " + value,
-                    code: 400
-                })
-            }
-        })
-        const existUser = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                [Op.or]: [
-                    { email: { [Op.eq]: req.body.email } },
-                    { phone: { [Op.eq]: req.body.phone } }
-                ]
-            }
-        })
-        if (existUser) {
-            return res.status(400).send({ message: "Email / No Telepon Telah Terdaftar!" })
-        }
-        const salt = await bcrypt.genSalt(10)
-        const password = await bcrypt.hash(req.body.password, salt)
-        const payload = {
-            ...req.body,
-            partner_code: req.header('x-partner-code'),
-            password: password,
-            verified: req.body.role == "customer" ? 0 : 1,
-            email_otp: null
-        };
-        const result = await users.create(payload)
-        return res.status(200).send({
-            status: "success",
-            items: result,
-            code: 200
-        })
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: "Server mengalami gangguan!", error: error })
-        return
+  try {
+    const requiredFields = ["name", "password", "role"];
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).send({
+          status: "error",
+          error_message: "Parameter tidak lengkap " + field,
+          code: 400,
+        });
+      }
     }
+    const existUser = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        [Op.or]: [
+          { email: { [Op.eq]: req.body.email } },
+          { phone: { [Op.eq]: req.body.phone } },
+        ],
+      },
+    });
+    if (existUser) {
+      return res
+        .status(400)
+        .send({ message: "Email / No Telepon Telah Terdaftar!" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+    const payload = {
+      ...req.body,
+      partner_code: req.header("x-partner-code"),
+      password: password,
+      verified: req.body.role == "customer" ? 0 : 1,
+      email_otp: null,
+    };
+    const result = await users.create(payload);
+    return res.status(200).send({
+      status: "success",
+      items: result,
+      code: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Server mengalami gangguan!", error: error });
+    return;
+  }
 };
 
 exports.update = async (req, res) => {
-    try {
-        const result = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.body.id }
-            }
-        })
-        if (!result) {
-            return res.status(400).send({ message: "Data tidak ditemukan!" })
-        }
-        let payload = {}
-        if (req.body.password && req.body.password !== "") {
-            const salt = await bcrypt.genSalt(10)
-            const password = await bcrypt.hash(req.body.password, salt)
-            payload = {
-                ...req.body,
-                password: password
-            }
-        } else {
-            payload = {
-                ...req.body
-            }
-        }
-        const onUpdate = await users.update(payload, {
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.body.id }
-            }
-        })
-        res.status(200).send({ message: "Berhasil ubah data", update: onUpdate })
-        return
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({ message: "Gagal mendapatkan data admin", error: error })
+  try {
+    const result = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        id: { [Op.eq]: req.body.id },
+      },
+    });
+    if (!result) {
+      return res.status(400).send({ message: "Data tidak ditemukan!" });
     }
-}
+    let payload = {};
+    if (req.body.password && req.body.password !== "") {
+      const salt = await bcrypt.genSalt(10);
+      const password = await bcrypt.hash(req.body.password, salt);
+      payload = {
+        ...req.body,
+        password: password,
+      };
+    } else {
+      payload = {
+        ...req.body,
+      };
+    }
+    const onUpdate = await users.update(payload, {
+      where: {
+        deleted: { [Op.eq]: 0 },
+        id: { [Op.eq]: req.body.id },
+      },
+    });
+    res.status(200).send({ message: "Berhasil ubah data", update: onUpdate });
+    return;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: "Gagal mendapatkan data admin", error: error });
+  }
+};
 
 exports.delete = async (req, res) => {
-    try {
-        const result = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.query.id }
-            }
-        })
-        if (!result) {
-            return res.status(404).send({ message: "Data tidak ditemukan!" })
-        }
-        result.deleted = 1
-        await result.save()
-        res.status(200).send({ message: "Berhasil hapus data" })
-        return
-    } catch (error) {
-        return res.status(500).send({ message: "Gagal mendapatkan data admin", error: error })
+  try {
+    const result = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        id: { [Op.eq]: req.query.id },
+      },
+    });
+    if (!result) {
+      return res.status(404).send({ message: "Data tidak ditemukan!" });
     }
-}
+    result.deleted = 1;
+    await result.save();
+    res.status(200).send({ message: "Berhasil hapus data" });
+    return;
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Gagal mendapatkan data admin", error: error });
+  }
+};
 
 exports.login = async (req, res) => {
-    try {
-        const { identity, password } = req.body;
-        if (!identity || !password) {
-            return res.status(404).send({ message: "Masukkan Email / No Telepon dan Password!" })
-        }
-        const result = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                status: { [Op.eq]: 1 },
-                [Op.or]: {
-                    phone: req.body.identity,
-                    email: req.body.identity
-                }
-            },
-        })
-        if (!result) {
-            return res.status(404).send({ message: "Akun Belum Terdaftar!" })
-        }
-        const isCompare = await bcrypt.compare(password, result.password)
-        if (!isCompare) {
-            return res.status(404).send({ message: "Password Salah!" })
-        }
-        const result2 = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                status: { [Op.eq]: 1 },
-                [Op.or]: {
-                    phone: req.body.identity,
-                    email: req.body.identity
-                },
-            },
-            attributes: { exclude: ['deleted', 'password'] },
-        })
-        return res.status(200).send({ message: "Berhasil Login", user: result2 })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({ message: "Gagal mendapatkan data admin", error: error })
+  try {
+    const { identity, password } = req.body;
+    if (!identity || !password) {
+      return res
+        .status(404)
+        .send({ message: "Masukkan Email / No Telepon dan Password!" });
     }
-}
+    const result = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        status: { [Op.eq]: 1 },
+        [Op.or]: {
+          phone: req.body.identity,
+          email: req.body.identity,
+        },
+      },
+    });
+    if (!result) {
+      return res.status(404).send({ message: "Akun Belum Terdaftar!" });
+    }
+    const isCompare = await bcrypt.compare(password, result.password);
+    if (!isCompare) {
+      return res.status(404).send({ message: "Password Salah!" });
+    }
+    const result2 = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        status: { [Op.eq]: 1 },
+        [Op.or]: {
+          phone: req.body.identity,
+          email: req.body.identity,
+        },
+      },
+      attributes: { exclude: ["deleted", "password"] },
+    });
+    return res.status(200).send({ message: "Berhasil Login", user: result2 });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: "Gagal mendapatkan data admin", error: error });
+  }
+};
 
 exports.loginbygoogle = async (req, res) => {
-    try {
-        const { email, phoneNumber, uid, displayName, photoURL } = req.body;
-        if (!email || !uid) {
-            return res.status(400).send({ message: "Parameter tidak lengkap!" })
-        }
-        const result = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                status: { [Op.eq]: 1 },
-                email: { [Op.eq]: email },
-                google_id: { [Op.eq]: uid }
-            },
-        })
-        const payload = {
-            ...req.body,
-            partner_code: req.header('x-partner-code'),
-            password: "loginbygoogle",
-            google_id: uid,
-            name: displayName,
-            phone: phoneNumber || null,
-            role: 'customer',
-            image: photoURL || null
-        };
-        const existEmail = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                status: { [Op.eq]: 1 },
-                email: { [Op.eq]: email }
-            },
-        })
-        if (existEmail) {
-            await users.update({ google_id: uid }, {
-                where: {
-                    deleted: { [Op.eq]: 0 },
-                    id: { [Op.eq]: existEmail.id }
-                }
-            })
-        }
-        let newUser = null;
-        if (!result && !existEmail) {
-            newUser = await users.create(payload)
-        }
-        return res.status(200).send({ message: "Berhasil Login", user: result || existEmail || newUser })
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({ message: "Gagal mendapatkan data admin", error: error })
+  try {
+    const { email, phoneNumber, uid, displayName, photoURL } = req.body;
+    if (!email || !uid) {
+      return res.status(400).send({ message: "Parameter tidak lengkap!" });
     }
-}
+    const result = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        status: { [Op.eq]: 1 },
+        email: { [Op.eq]: email },
+        google_id: { [Op.eq]: uid },
+      },
+    });
+    const payload = {
+      ...req.body,
+      partner_code: req.header("x-partner-code"),
+      password: "loginbygoogle",
+      google_id: uid,
+      name: displayName,
+      phone: phoneNumber || null,
+      role: "customer",
+      image: photoURL || null,
+    };
+    const existEmail = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        status: { [Op.eq]: 1 },
+        email: { [Op.eq]: email },
+      },
+    });
+    if (existEmail) {
+      await users.update(
+        { google_id: uid },
+        {
+          where: {
+            deleted: { [Op.eq]: 0 },
+            id: { [Op.eq]: existEmail.id },
+          },
+        }
+      );
+    }
+    let newUser = null;
+    if (!result && !existEmail) {
+      newUser = await users.create(payload);
+    }
+    return res
+      .status(200)
+      .send({
+        message: "Berhasil Login",
+        user: result || existEmail || newUser,
+      });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: "Gagal mendapatkan data admin", error: error });
+  }
+};
 
 exports.verificationResetPassword = async (req, res) => {
-    try {
-        const result = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.body.id },
-                email: { [Op.eq]: req.body.email },
-                reset_otp: { [Op.eq]: req.body.otp },
-            }
-        })
-        if (!result) {
-            return res.status(400).send({ message: "Kode OTP Salah!" })
-        }
-        const onUpdate = await users.update({
-            reset_otp: null
-        }, {
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.body.id }
-            }
-        })
-        res.status(200).send({ message: "Verifikasi Berhasil", update: onUpdate })
-        return
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({ message: "Gagal mendapatkan data admin", error: error })
+  try {
+    const result = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        id: { [Op.eq]: req.body.id },
+        email: { [Op.eq]: req.body.email },
+        reset_otp: { [Op.eq]: req.body.otp },
+      },
+    });
+    if (!result) {
+      return res.status(400).send({ message: "Kode OTP Salah!" });
     }
-}
+    const onUpdate = await users.update(
+      {
+        reset_otp: null,
+      },
+      {
+        where: {
+          deleted: { [Op.eq]: 0 },
+          id: { [Op.eq]: req.body.id },
+        },
+      }
+    );
+    res.status(200).send({ message: "Verifikasi Berhasil", update: onUpdate });
+    return;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: "Gagal mendapatkan data admin", error: error });
+  }
+};
 
 exports.verificationRegistration = async (req, res) => {
-    try {
-        const result = await users.findOne({
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.body.id },
-                email: { [Op.eq]: req.body.email },
-                email_otp: { [Op.eq]: req.body.otp },
-            }
-        })
-        if (!result) {
-            return res.status(400).send({ message: "Kode OTP Salah!" })
-        }
-        const onUpdate = await users.update({
-            email_otp: null,
-            verified: 1
-        }, {
-            where: {
-                deleted: { [Op.eq]: 0 },
-                id: { [Op.eq]: req.body.id }
-            }
-        })
-        res.status(200).send({ message: "Verifikasi Berhasil", update: onUpdate })
-        return
-    } catch (error) {
-        console.log(error);
-        return res.status(500).send({ message: "Gagal mendapatkan data admin", error: error })
+  try {
+    const result = await users.findOne({
+      where: {
+        deleted: { [Op.eq]: 0 },
+        id: { [Op.eq]: req.body.id },
+        email: { [Op.eq]: req.body.email },
+        email_otp: { [Op.eq]: req.body.otp },
+      },
+    });
+    if (!result) {
+      return res.status(400).send({ message: "Kode OTP Salah!" });
     }
-}
+    const onUpdate = await users.update(
+      {
+        email_otp: null,
+        verified: 1,
+      },
+      {
+        where: {
+          deleted: { [Op.eq]: 0 },
+          id: { [Op.eq]: req.body.id },
+        },
+      }
+    );
+    res.status(200).send({ message: "Verifikasi Berhasil", update: onUpdate });
+    return;
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .send({ message: "Gagal mendapatkan data admin", error: error });
+  }
+};
